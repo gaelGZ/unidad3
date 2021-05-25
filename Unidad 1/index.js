df7652b118b424fa91c5d1897a5f3a69bd782d0a
Gael <cordovamirelesgael@gmail.com>
2021-05-25 09:55:49 -0500

    segundo

---
+++ Unidad 1/index.js
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Ejemplo estructura condicional IF si
+</title>
+<body bgcolor="cyan">
+<script type="text/javascript">
+var d1=prompt("Introduce la calificacion 1 ");
+var d2=prompt("Introduce la calificacion 2 ");
+var d3=prompt("Introduce la calificacion 3 ");
+var p=(d1+d2+d3)/3;
+
+if (vp<=6) {
+	alert("Paso...");
+	document.write("Paso...");
+} else {
+	alert("No paso...");
+	document.write("No paso...");
+}
+</body>
+</head>
+</html>
